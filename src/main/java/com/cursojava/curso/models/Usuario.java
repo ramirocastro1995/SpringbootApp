86b512686e907511ca02eb99ea1d35acfd03a059
Ramiro <ramirosalvadrocastro95@hotmail.com>
2023-04-03 17:32:26 -0300

    Controllers test

--- src/main/java/com/cursojava/curso/models/Usuario.java
+++
@@ -1,6 +0,0 @@
-package com.cursojava.curso.models;
-
-public class Usuario {
-    private String nombre;
-    private String dni;
-}
--- src/test/java/com/example/demo/ApplicationTests.java
+++ src/test/java/com/example/demo/ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.cursojava.curso;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CursoApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
